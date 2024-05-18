ca81a61bca217f38030937d5cad475d9578454a3
Eduardo Aguiar <eduagmart@gmail.com>
2024-05-18 00:22:21 -0300

    Setup

---
+++ app/pages/index.tsx
@@ -0,0 +1,14 @@
+// app/pages/index.tsx
+
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to PowerSchool</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+};
+
+export default HomePage;
